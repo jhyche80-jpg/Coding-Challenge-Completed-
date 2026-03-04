0579c48133391a2fa7c657cd9ce25661fc82d9a8
Javon <jhyche80@gmail.com>
2026-03-04 15:50:43 -0500

    I added a defer to the scripting so the code will always go back to the script

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
-
-
+const inputName = document.getElementById('name')
+const inputEmail = document.getElementById('email')
+const errorName = document.getElementById('errorName')
+const contact = document.getElementById('contact-form')
+const submitBtn = document.getElementById
 document.addEventListener('DOMContentLoaded', function () {
 	const menu = document.getElementById('menu');
 	const menuBtn = document.getElementById('menu-btn');
@@ -12,13 +15,22 @@ document.addEventListener('DOMContentLoaded', function () {
 	menuBtn.addEventListener('click', toggleMenu);
 });
 
-document.getElementById('contact-form').addEventListener('submit', function (event) {
+contact.addEventListener('submit', function (event) {
 	event.preventDefault();
+	if (inputName.innerText.trim() === "") {
+		errorName.style.display = 'inline'
+
+	}
+
 	alert('Form submitted!');
 });
 
 window.onscroll = function () {
 	if (window.scrollY > 500) {
+		document.getElementById('back-to-top').style.display = "none";
+	} else {
 		document.getElementById('back-to-top').style.display = "block";
 	}
+
+
 };
