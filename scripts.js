05f8c26cea67fd5dd5cba116088c68ebe73e863f
Javon <jhyche80@gmail.com>
2026-03-05 06:20:13 -0500

    I made the toggle fro the  menu button

--- scripts.js
+++ scripts.js
@@ -2,15 +2,22 @@ const inputName = document.getElementById('name')
 const inputEmail = document.getElementById('email')
 const contact = document.getElementById('contact-form')
 const emailError = document.getElementById('emailError')
-const submitBtn = document.getElementById
+
 document.addEventListener('DOMContentLoaded', function () {
 	const menu = document.getElementById('menu');
 	const menuBtn = document.getElementById('menu-btn');
 
 	function toggleMenu() {
 		console.log('button clicked ')
+		const img = document.getElementById('menuImg');
 
-		menu.classList.toggle('visible')
+		if (img.src.includes('bars.svg')) {
+			img.src = './assets/xmark.svg';
+		} else {
+			img.src = './assets/bars.svg';
+		}
+
+		menu.classList.toggle('visible');
 	}
 
 	menuBtn.addEventListener('click', toggleMenu);
